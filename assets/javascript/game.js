1c0db386ae5b838d2cb4c2fbd3a100b4d1c27ab9
Carmen SanDiego <efemar82@gmail.com>
2019-09-06 00:24:48 -0700

    includes code for one answer allowed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,20 +16,68 @@
 
 $(document).ready(function () {
 
-    $("#button").on("click", function () {
-   
+
+    $("#start").on("click", function() {
+        
+        // Show #quiz-container
+        $("#quiz-container").removeClass('hidden');
+
+    
+        //Start the timer
+
+       
+
+
+    })
+    
+
+    $(".question1").change(function () {
+        $('.question1').not(this).prop('checked', false);
+     });
+
+     $(".question2").change(function () {
+        $('.question2').not(this).prop('checked', false);
+     });
+
+     $(".question3").change(function () {
+        $('.question3').not(this).prop('checked', false);
+     });
+
+     $(".question4").change(function () {
+        $('.question4').not(this).prop('checked', false);
+     });
+
+     $(".question5").change(function () {
+        $('.question5').not(this).prop('checked', false);
+     });
+
+     $(".question6").change(function () {
+        $('.question6').not(this).prop('checked', false);
+     });
+
+
+    $("#results").on("click", function () {
+
         //creating variables to store each question's answers
-    var question1 = document.quiz.question1.value
-    var question2 = document.quiz.question2.value
-    var question3 = document.quiz.question3.value
-    var question4 = document.quiz.question4.value
-    var question5 = document.quiz.question5.value
-    var question6 = document.quiz.question6.value
+        
+        
+        var question1 = $("input#mc.question1").val();
+       
+        var question2 = $("input#mc.question2").val();
+        
+        var question3 = $("input#mc.question3").val();
+        
+        var question4 = $("input#mc.question4").val();
+        
+        var question5 = $("input#mc.question5").val();
+        
+        var question6 = $("input#mc.question6").val();
+       
+        console.log(question1,question2, question3, question4, question5, question6);
 
-
-    var correctAns = 0
-    var incorrectAns = 6-correctAns;
-    var unAns = 6-correctAns-incorrectAns;
+        var correctAns = 0
+        var incorrectAns = 6 - correctAns;
+        var unAns = 6 - correctAns - incorrectAns;
 
         if (question1 === "Christina Applegate") {
             correctAns++;
@@ -49,42 +97,42 @@ $(document).ready(function () {
         if (question6 === "5") {
             correctAns++
         }
-    
-    var messages=["I might have to unfriend you", "Alright..You can do a little better", "Holy Moly!! You are basically part of the Friends Gang!"]
-    
-    var gifs=["https://media.giphy.com/media/Js1ur5v1vmaj4mRxyq/giphy.gif","https://media.giphy.com/media/eJS4WUQ7MkNKx3qxPN/giphy.gif","https://media.giphy.com/media/YnBntKOgnUSBkV7bQH/giphy.gif"]
-    
-    var score;
-        if (correctAns <=1) {
-            score=1;
+
+        var messages = ["I might have to unfriend you!!", "Alright..You can do a little better", "Holy Moly!! You are basically part of the Friends Gang!"]
+
+        var gifs = ["https://media.giphy.com/media/Js1ur5v1vmaj4mRxyq/giphy.gif", "https://media.giphy.com/media/eJS4WUQ7MkNKx3qxPN/giphy.gif", "https://media.giphy.com/media/YnBntKOgnUSBkV7bQH/giphy.gif"]
+
+        var score;
+        if (correctAns <= 1) {
+            score = 0;
         }
-        if (correctAns >1 && correctAns <=3) {
-            score=2;
+        if (correctAns > 1 && correctAns <= 3) {
+            score = 1;
         }
-        if (correctAns >=4) {
-            score=3;
+        if (correctAns >= 4) {
+            score = 2;
         }
 
 
-$("#message").text(message[score]);
-$("#cor-ans").text("Correct: " + correctAns);
-$("#inc-ans").text("Incorrect: " + incorrectAns);
-$("#un-ans").text("Unaswered: " + unAns);
-$("#gifs").html("<img src=[gifs[score]]>")
+        $('#quiz-container').addClass('hidden');
+        $("#message").text(messages[score]);
+        $("#cor-ans").text("Correct: " + correctAns);
+        $("#inc-ans").text("Incorrect: " + incorrectAns);
+        $("#un-ans").text("Unaswered: " + unAns);
+        $("#gifs").html("<img src="+gifs[score]+">")
+    })
 
-$("#button").on("click", function () {
+
+
+
 
 
 }
-
-})
-    
-
+)
 //TODO LIST
-//Create an action for the start button to show the questions
-//Create an action for the submit answers button to load results
-//
-    
+
+//Create a function that resets the game
+
 
 
 
