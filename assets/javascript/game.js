6c9ec2d50ad16d9d546d2a4c72d9b1831882a483
Carmen SanDiego <efemar82@gmail.com>
2019-09-05 20:52:16 -0700

    Included first part of pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+
+//Start Button that starts the time countdown
+
+// Need to create a form input for each question
+
+//Need to provide asnwers for each question
+
+
+//Pick gifs or images for getting everything correct
+    // Happy: https://media.giphy.com/media/YnBntKOgnUSBkV7bQH/giphy.gif
+    // Average: https://media.giphy.com/media/eJS4WUQ7MkNKx3qxPN/giphy.gif
+    // Bad: https://media.giphy.com/media/Js1ur5v1vmaj4mRxyq/giphy.gif
+
+    //Questions:
+    //Q: Which actress played Amy, Rachel's sister?
+    //A: Christina Applegate (y), Jennifer Aniston, Lisa Kudrow, Selma Blair
+
+    //Q: What was Ross' first wife's name?
+    //A: Susan, Emily, Carol (y), Rachel
+
+    //Q: Who pees on monica's leg after she gets stung by a jellyfish
+    //A: Phoebe, Rachel, Chandler (y), Joey
+
+    //Q: What was Chandler's job in he early seasons of the show?
+    //A: Data Analyst, IT procurements manager (y), Marketing Manager, Anthropologist
+    
+    //Q: What was the name for Joey's stuffed animal?
+    //A: Marcel, Alisha May, Hugsy(y), Muriel
+
+    //Q: Which apartment number did Monica originally have?
+    //A: 4, 5 (Y), 17, 20 
+
+
+    
